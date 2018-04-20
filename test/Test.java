4cb10d5bd0dd96d2ef86d0a6612973077c97a7f5
Jack Clarke <tokyojack2001@yahoo.com>
2018-04-20 07:10:46 +0000

    Add event register

--- test/Test.java
+++ test/Test.java
@@ -1,16 +1,16 @@
-package me.tokyojack.spigot.airdrop;
+package package;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.valeon.core.events.PlayerKillPlayerEvent;
-
 public class Test extends JavaPlugin implements Listener {
 
 	public void onEnable() {
 		PlayerKillPlayerEvent.register(this);
+		
+		Bukkit.getServer().getPluginManager().registerEvents(this, this);
 	}
 
 	@EventHandler
